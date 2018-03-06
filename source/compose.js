b5a156a7cdbe227c8b64e359cde87c3831f3bad9
felichio <fsafaridis@hotmail.com>
2018-03-06 19:32:12 +0200

    Fixed return statement on compose function

--- source/compose.js
+++ source/compose.js
@@ -4,6 +4,7 @@ let reduce = require("./reduce.js");
 
 const compose = function compose(...args) {
     let composed = reduce((composedFn, currentFn) => x => composedFn(currentFn(x)), args);
+    return composed;
 };
 
 module.exports = compose;
