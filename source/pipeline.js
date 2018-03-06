2d3ea26aaff6bcd36d13409cf61750700254e27d
felichio <fsafaridis@hotmail.com>
2018-03-06 19:50:32 +0200

    Add pipeline function

---
+++ source/pipeline.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let reduce = require("./reduce.js");
+
+const pipeline = function pipeline(...args) {
+    let pipelined = reduce((composedFn, currentFn) => x => currentFn(composedFn(x)), args);
+    return pipelined;
+}
+
+module.exports = pipeline;
