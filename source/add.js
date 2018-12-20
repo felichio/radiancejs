9bfc1969f34c4c4f95b1d0789390caa98e820488
Felix <fsafaridis@hotmail.com>
2018-12-20 22:11:36 +0200

    Add add function

---
+++ source/add.js
@@ -0,0 +1,12 @@
+/**
+ * Adds two values. op (+)
+ * @signature
+ *      (Number, Number) -> Number *      
+ * @example
+ *      let result = add(2, 5);  //=> 7
+ */
+
+const add = (x, y) => x + y;
+
+
+export default add;
