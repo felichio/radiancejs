4275e77a2f54eb4a894ee0d9c14a22ccbc6476c4
Felix <fsafaridis@hotmail.com>
2018-12-20 22:40:57 +0200

    Add divide function

---
+++ source/divide.js
@@ -0,0 +1,12 @@
+/**
+ * Divides second parameter by first one. op (/)
+ * @signature
+ *      (Number, Number) -> Number
+ * @example
+ *      let result = divide(2, 5);  //=> 0.4
+ */
+
+const divide = (x, y) => x / y;
+
+
+export default divide;
