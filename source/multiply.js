171db93992a0f0f5e3290f099007b8f62afb788f
Felix <fsafaridis@hotmail.com>
2018-12-20 22:40:44 +0200

    Add multiply function

---
+++ source/multiply.js
@@ -0,0 +1,12 @@
+/**
+ * Multiplies first parameter, second parameter times . op (*)
+ * @signature
+ *      (Number, Number) -> Number
+ * @example
+ *      let result = multiply(2, 5);  //=> 10
+ */
+
+const multiply = (x, y) => x * y;
+
+
+export default multiply;
