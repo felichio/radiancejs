c2b9c73756b215c198f6ae1d25ca42a953ac0627
Felix <fsafaridis@hotmail.com>
2018-12-20 22:11:47 +0200

    Add subtract function

---
+++ source/subtract.js
@@ -0,0 +1,12 @@
+/**
+ * Subtracts second parameter from first one. op (-)
+ * @signature
+ *      (Number, Number) -> Number
+ * @example
+ *      let result = subtract(2, 5);  //=> -3
+ */
+
+const subtract = (x, y) => x - y;
+
+
+export default subtract;
