69eea2bbd63f709e6f0d89372f97d8d90ab04822
Felix <fsafaridis@hotmail.com>
2018-12-21 17:19:24 +0200

    Add curry function implementation

---
+++ source/curry.js
@@ -0,0 +1,20 @@
+/**
+ * Converting a multivariable function into a stepwise sequence of unary functions
+ * @signature
+ *      ((n1, n2, ...nm) -> n) -> n1 -> n2 -> ...nm -> n 
+ * @example
+ *      
+ */
+
+
+// Implementation utilizing Function.prototype.bind method
+const curryWithBind = (fn, argLength = fn.length) => argLength === 0 ? fn() : x => curryWithBind(fn.bind(null, x), argLength - 1);
+
+// Default implementation via recursion and closures
+const curryRecursive = (fn, argLength = fn.length) => {
+    const innerCurry = (...args) => y => (argLength <= args.length + 1 ? fn : innerCurry)(...args, y);
+    return innerCurry();
+};
+
+
+export default curryRecursive;
