b62cc76aad4e286c3e9bc18f7faf136782068d60
Felix <fsafaridis@hotmail.com>
2018-12-21 17:18:40 +0200

    Binary operation function {+, -, ...}

---
+++ source/binaryOp.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ * Performs a binary Operation between to arguments. Set{"+", "-", "/", "*", "=", ">", "<"}
+ * @signature
+ *      (String, Number, Number) -> Number || Boolean
+ * 
+ * @example
+ * 
+ * 
+ * 
+ */
+
+// Default. Implementation utilizing switch block statement
+const switchImplementation = (operator, x, y) => {
+    switch(operator) {
+        case "+": return x + y;
+        case "-": return x - y;
+        case "/": return x / y;
+        case "*": return x * y;
+        case "=": return x === y;
+        case ">": return x > y;
+        case "<": return x < y;
+    }
+};
+
+// Implementation utilizing if statement
+const ifImplementation = (operator, x, y) => {
+    if (operator === "+") return x + y;
+    else if (operator === "-") return x - y;
+    else if (operator === "/") return x / y;
+    else if (operator === "*") return x * y;
+    else if (operator === "=") return x === y;
+    else if (operator === ">") return x > y;
+    else if (operator === "<") return x < y;
+}
+
+
+
+export default switchImplementation;
+
