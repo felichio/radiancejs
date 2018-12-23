89b43449709d9168ed7cdbdd85a9599ece5cb781
Felix <fsafaridis@hotmail.com>
2018-12-23 15:58:45 +0200

    ID function implementation

---
+++ source/identity.js
@@ -0,0 +1,13 @@
+/**
+ * @description
+ *  Identity function. Returns the first passed argument
+ * @signature
+ *  a -> a
+ * @example
+ *  
+ */
+
+
+const identity = x => x;
+
+export default identity;
