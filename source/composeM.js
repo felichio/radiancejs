2d9ca1e31e92c1dd7db9f2e0bbd83a064429e65c
Felix <fsafaridis@hotmail.com>
2019-06-06 00:04:33 +0300

    multiple args composition and pipelining implementation

---
+++ source/composeM.js
@@ -0,0 +1,30 @@
+/**
+ * @description
+ *  Function composition. For f: A -> B, g: B -> C function composition produces a new function
+ *  with a mapping A -> C and type gྪ ◦ f = g(f(x)) 
+ * @signature
+ *  compose :: (y -> z, x -> y, ..., b -> c, a -> b) -> a -> z 
+ * @example
+ *  add5 :: Number -> Number
+ *  const add5 = x => x + 5;
+ *
+ *  mul50 :: Number -> Number
+ *  const mul50 = x => x * 50;
+ *
+ *  mul50AndAdd5 :: Number -> Number
+ *  const mul50AndAdd5 = compose(add5, mul50) == add5(mul50)
+ *
+ *  mul50AndAdd5(10) = add5(mul50(10)) = 505
+ *  
+ */
+
+
+import { foldl, identity, foldr } from "./exporter";
+
+const composeTwo = (g, f) => (...args) => g(f(...args));
+
+const composeNleft = (...args) => foldl(composeTwo)(identity)(args);
+
+const composeNright = (...args) => foldr(composeTwo)(identity)(args);
+
+export default composeNleft;
---
+++ source/pipelineM.js
@@ -0,0 +1,30 @@
+/**
+ * @description
+ *  Function pipelining. For f: A -> B, g: B -> C function pipelining produces a new function
+ *  with a mapping A -> C and type f | g  = g(f(x)) 
+ * @signature
+ *  pipeline :: (a -> b, b -> c, ... , x -> y, y -> z) -> a -> z
+ * @example
+ *  add5 :: Number -> Number
+ *  const add5 = x => x + 5;
+ *
+ *  mul50 :: Number -> Number
+ *  const mul50 = x => x * 50;
+ *
+ *  mul50AndAdd5 :: Number -> Number
+ *  const mul50AndAdd5 = pipeline(mul50, add5) == add5(mul50)
+ *
+ *  mul50AndAdd5(10) = add5(mul50(10)) = 505
+ *  
+ */
+
+
+import { foldl, identity, foldr } from "./exporter";
+
+const pipelineTwo = (f, g) => (...args) => g(f(...args));
+
+const pipelineNleft = (...args) => foldl(pipelineTwo)(identity)(args);
+
+const pipelineNright = (...args) => foldr(pipelineTwo)(identity)(args);
+
+export default pipelineNleft;
