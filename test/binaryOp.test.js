0320d5049688fc69da198f6d12c726dbabf8df16
Felix <fsafaridis@hotmail.com>
2019-06-11 19:42:58 +0300

    changed test.binaryOp to reflext flipped parameter definition

--- test/binaryOp.test.js
+++ test/binaryOp.test.js
@@ -4,16 +4,16 @@ test("function binaryOp + -> binaryOp('+', 2, 3) = 5", () => {
     return expect(r.binaryOp("+", 2, 3)).toBe(5);
 });
 
-test("function binaryOp - -> binaryOp('+', 2, 3) = -1", () => {
-    return expect(r.binaryOp("-", 2, 3)).toBe(-1);
+test("function binaryOp - -> binaryOp('-', 2, 3) = 1", () => {
+    return expect(r.binaryOp("-", 2, 3)).toBe(1);
 });
 
-test("function binaryOp > -> binaryOp('>', 2, 3) = false", () => {
-    return expect(r.binaryOp(">", 2, 3)).toBe(false);
+test("function binaryOp > -> binaryOp('>', 2, 3) = true", () => {
+    return expect(r.binaryOp(">", 2, 3)).toBe(true);
 });
 
-test("function binaryOp < -> binaryOp('>', 2, 3) = true", () => {
-    return expect(r.binaryOp("<", 2, 3)).toBe(true);
+test("function binaryOp < -> binaryOp('>', 2, 3) = false", () => {
+    return expect(r.binaryOp("<", 2, 3)).toBe(false);
 });
 
 const mulOf5 = r.binaryOp("*")(5);
